183d90c98badb716b74b7b7e240327e85e7df76b
SangEun Lee <d4space@gmail.com>
2015-12-02 13:59:13 +0900

    hahahaha

--- EnvSetup/EOSread.sh
+++ EnvSetup/EOSread.sh
@@ -1,2 +1,7 @@
 # CERNBox
+# portal: cernbox.cern.ch
+# Help & Download Clients
+#  Frequently Asked Questions and Knowledge Base Articles
+# CERNBox quick tutorial for beginners
+
 /afs/cern.ch/project/eos/installation/0.3.84-aquamarine.user/bin/eos.select
