3ba20e59efb64680641001a74e312695f9166ef1
SangEun Lee <d4space@gmail.com>
2016-01-10 16:54:02 +0900

    hahahaha

--- Miscell/genTable_cfg.py
+++ Miscell/genTable_cfg.py
@@ -1,22 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("wNTuple")
-process.load("TerraNova.NtupleMaker.wAnalysis_MC_cff")
+process = cms.Process("GenCheck")
+#process.load("TerraNova.NtupleMaker.wAnalysis_MC_cff")
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
 #process.MessageLogger = cms.Service("MessageLogger")
 #process.MessageLogger.destinations = ['cout']
 #process.MessageLogger.cout = cms.untracked.PSet(
 #    threshold = cms.untracked.string('INFO'),
 #    FwkReport = cms.untracked.PSet(reportEvery=cms.untracked.int32(1000))
 #)
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring('file:../../../../RunIIData/testSample/WZJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv1/006DEE0F-A49F-E511-B06C-0CC47A009E26.root'),
 )
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -25,7 +25,7 @@ process.printTree = cms.EDAnalyzer("ParticleListDrawer",
     printVertex = cms.untracked.bool(False),
     src = cms.InputTag("genParticles")
     )
-process.load("TerraNova.NtupleMaker.Sources.WplusToMuNu_S8_8TeV_AODSIM_PatSkim_local_cff")
+#process.load("TerraNova.NtupleMaker.Sources.WplusToMuNu_S8_8TeV_AODSIM_PatSkim_local_cff")
 
 process.p = cms.Path(
     process.printTree
