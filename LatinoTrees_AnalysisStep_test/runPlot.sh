e0690662c56b3c10024c163d69ca0ec4bb6cfb36
SangEun Lee <d4space@gmail.com>
2016-01-29 16:10:31 +0900

    hahahaha

--- LatinoTrees_AnalysisStep_test/runPlot.sh
+++ LatinoTrees_AnalysisStep_test/runPlot.sh
@@ -1,4 +1,5 @@
 #root -l WZJets_13TeV.root DrawRange.cxx\(\"m2MuFromZorGstar\",50,0,20\)
 #root -l latino_stepB_numEvent100.root DrawRange.cxx\(\"Gen_ZGstar_deltaR\",50,0,4\)
-root -l latino_stepB_numEvent100.root DrawRange.cxx\(\"Gen_ZGstar_mass\",50,0,100\)
+root -l /u/user/sangilpark/RunIIData/CernBoxHWW2015/latino_Run2015D_05Oct2015_SingleMuon_0000__part0.root  DrawRange.cxx\(\"Gen_ZGstar_mass\",50,0,100\)
+#root -l latino_stepB_numEvent100.root DrawRange.cxx\(\"Gen_ZGstar_mass\",50,0,100\)
 #root -l WZJets_13TeV.root DrawRange.cxx\(\"ZGstarDimu_DelaR\",50,0,4\)
