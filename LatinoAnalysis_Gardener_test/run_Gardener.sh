77088b0b6713111e6835138363fe2817cadcedd0
SangEun Lee <d4space@gmail.com>
2016-03-22 19:24:04 +0900

    hahahah

---
+++ LatinoAnalysis_Gardener_test/run_Gardener.sh
@@ -0,0 +1,9 @@
+
+# isWgsLepton --kind 3 \ 
+#gardener.py l2selfiller \
+#		--kind 3 --cmssw 763\
+#		latino_stepB_numEvent1000.root \
+#		latino_stepB_numEvent1000_garden.root
+gardener.py genvariablesfiller \
+		latino_stepB_numEvent100.root \
+		latino_stepB_numEvent100_gen.root
