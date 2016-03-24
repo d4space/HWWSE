3bdb014af451b76eeb0975ee5211ba7153e751d2
Sangil <sangilpark@knu.ac.kr>
2016-03-24 10:29:57 +0100

    haha

--- LatinoAnalysis_Gardener_test/run_Gardener_batch.sh
+++ LatinoAnalysis_Gardener_test/run_Gardener_batch.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# login : cms02.knu.ac.kr , lxplus.cern.ch
+# Andrea's pre-process ntuple location : /eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/LatinoTrees/
+# To use Cernbox, /afs/cern.ch/project/eos/installation/0.3.84-aquamarine.user/bin/eos.select -b fuse mount ~/eos
+# Sangil Cernbox location : /eos/user/s/spak/HWW2015/
+
 # isWgsLepton --kind 3 \ 
 #gardener.py l2selfiller \
 #		--kind 3 --cmssw 763\
@@ -23,16 +28,16 @@ PWD=$(pwd)
 if [ ! -e $PostDir ]; then
   mkdir $PostDir
 fi
-
-for file in `ls $InputDir/ | grep root`;
+#for file in `ls $InputDir/ | grep root`;
+for file in `ls $InputDir/ | grep latino_ZZTo4L__part1.root`;
 do
-  echo "$file is going to be post-processed"
   echo "#!/bin/sh" >& Gardener_batch_$num.sh
   echo "export SCRAM_ARCH=slc6_amd64_gcc493" >> Gardener_batch_$num.sh
-  echo "export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch" >> Gardener_batch_$num.sh
-  echo "if [ -f $VO_CMS_SW_DIR/cmsset_default.sh ]; then" >> Gardener_batch_$num.sh
-  echo "  source $VO_CMS_SW_DIR/cmsset_default.sh">> Gardener_batch_$num.sh
+  echo "if [ -f /cvmfs/cms.cern.ch/cmsset_default.sh ]; then" >> Gardener_batch_$num.sh
+  echo "  source /cvmfs/cms.cern.ch/cmsset_default.sh">> Gardener_batch_$num.sh
   echo "fi">> Gardener_batch_$num.sh
+  #echo "eosmount ~/eos">> Gardener_batch_$num.sh # in lxplus
+
   echo "cd $CMSSW_BASE/src" >> Gardener_batch_$num.sh
   echo "cmsenv" >> Gardener_batch_$num.sh
   echo "" >> Gardener_batch_$num.sh
@@ -40,6 +45,8 @@ do
   echo "gardener.py genvariablesfiller \ $InputDir$file \ $PostDir/$file" >> Gardener_batch_$num.sh
   chmod u+x Gardener_batch_$num.sh
 
-  qsub -q short Gardener_batch_$num.sh
+  echo "$file is going to be post-processed"
+  #qsub -q short Gardener_batch_$num.sh # cms02.knu.ac.kr
+  #bsub -q 1nd -J Gardener_$num < Gardener_batch_$num.sh # -q 1nd (1 day), -J job1(job name) 
   num="$(($num+1))"
 done
