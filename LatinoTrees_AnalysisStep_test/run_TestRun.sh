cc67542afc5baf1e90055b8e4f0d25275f525034
Sangil <sangilpark@knu.ac.kr>
2016-04-18 12:18:24 +0200

    haha

--- LatinoTrees_AnalysisStep_test/run_TestRun.sh
+++ LatinoTrees_AnalysisStep_test/run_TestRun.sh
@@ -1,4 +1,12 @@
-./test-run_SE.sh 500 ~/eos/cms/store/relval/CMSSW_7_6_3_patch2/RelValZEE_13/MINIAODSIM/76X_mcRun2_asymptotic_v12_multiCoreTest3-v1/10000/BEA52424-18E0-E511-8852-0025905A6070.root
+### Sample for RelVal sample ###
+### RelVal Settings ###
+# 1. need to mount eos : $eosmount ~/eos
+# 2. check RelVal sample in eos : /eos/cms/store/relval/CMSSW~~/~~/~~.root
+# 3. check data global tag is used  
+# 4. run script like : $ ./test-run_SE_RelVal.sh 100 ~/eos/cms/store/relval/CMSSW_7_6_3_patch2/RelValZEE_13/MINIAODSIM/76X_mcRun2_asymptotic_v12_multiCoreTest3-v1/10000/BEA52424-18E0-E511-8852-0025905A6070.root
+./test-run_SE_RelVal.sh 500 ~/eos/cms/store/relval/CMSSW_7_6_3_patch2/RelValZEE_13/MINIAODSIM/76X_mcRun2_asymptotic_v12_multiCoreTest3-v1/10000/BEA52424-18E0-E511-8852-0025905A6070.root
+
+## Usual sample
 ./test-run_SE.sh 500 "/u/user/sangilpark/RunIIData/testSample/WGstarToLNuMuMu_012Jets_13TeV-madgraph/RunIIFall15MiniAODv2/1A55058C-32E5-E511-B619-0025905A48F0.root"
 #./test-run_SE.sh "/u/user/salee/Latino/DataRunII/Fall15MiniAODv2/WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/042C25F7-DBB8-E511-9178-2C600CAFEF7C.root"
 #./test-run_SE.sh 1000 "/d3/scratch/khakim/RunII/miniAODsamples/WZJets_TuneCUETP8M1_13TeV_amcatnloFXFX_pythia8_RunIIFall15MiniAODv2/00D30F00-1EB8-E511-A3A0-FA163E2815B2.root"
