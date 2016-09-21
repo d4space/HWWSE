d0b6035723daa14dee86be5aeff269eb342dc1cc
sangilpark <sangilpark@knu.ac.kr>
2016-09-21 16:10:27 +0900

    haha

---
+++ LatinoNtupleList/LatinoNtupleList_HWW6p3_21Jun2016_v2_Run2016B_PromptReco_l2loose__hadd__EpTCorr__l2tight.h
@@ -0,0 +1,41 @@
+
+void ConnectTree(TChain* tree, TString theSample, Bool_t runAtLxplus){
+
+  TString filesPathData;
+  
+  //filesPathData = "dcap://cluster142.knu.ac.kr//pnfs/knu.ac.kr/data/cms/store/user/spak/Latino_CernBox/80Xv2/07Jun2016_Run2016B_PromptReco/l2loose16bis__hadd__l2tight/";
+  //filesPathMC = "dcap://cluster142.knu.ac.kr//pnfs/knu.ac.kr/data/cms/store/user/spak/Latino_CernBox/80Xv2/07Jun2016_spring16__mAODv2/MCl2loose__hadd__bSFLepEff__l2tight/";
+  filesPathData = "/u/user/sangilpark/Latino_CernBox/80Xv2/HWW6p3/21Jun2016_v2_Run2016B_PromptReco/l2loose__hadd__EpTCorr__l2tight/";
+  
+  // l2sel
+  if (theSample == "DataRun2015") {
+  }else if (theSample == "DataRun2015_C") {
+  }else if (theSample == "DataRun2015_D") {
+  }else if (theSample == "DataRun2016_B") {
+    // Luminosity 2.791 fb^-1 from twiki,
+    // https://twiki.cern.ch/twiki/bin/viewauth/CMS/LatinosTreesRun2
+  	tree->Add( filesPathData + "latino_Run2016B_PromptReco_SingleMuon.root");
+  }else if (theSample == "DataRun2016_C") {
+  }else if (theSample == "WJetsLNu") {
+  }else if (theSample == "WJetsLNu_HT") { 
+  }else if (theSample == "ggWWto2L") {
+  }else if (theSample == "WWTo2L2Nu") {
+  }else if (theSample == "WZ") {
+  }else if (theSample == "WZ2Q") {
+  }else if (theSample == "WZ3LNu") {
+  }else if (theSample == "ZZ") {
+  }else if (theSample == "TTbar") {
+  }else if (theSample == "TW") {
+  }else if (theSample == "DY") {
+  }else if (theSample == "WgammaNoStar") {
+  }else if (theSample == "WgammaStarToLNuMuMu") {
+  }else if (theSample == "WgammaStarToLNuEE") {
+  }else if (theSample == "HWW125") { 
+  }else if (theSample == "Zgamma") { 
+  }
+  else {
+        cout<<"Check theSample name, exiting......................."<<endl;
+  	return;
+  }
+ 
+}
