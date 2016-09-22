a63bc9e57b885b8fdcc5029192e4b5a6fdfacd17
SangEun Lee <d4space@gmail.com>
2016-09-22 11:31:21 +0900

    hahaha

--- LatinoTrees_AnalysisStep_test/compare.C
+++ LatinoTrees_AnalysisStep_test/compare.C
@@ -10,36 +10,70 @@ void compare(){
   TFile *f_WZTo3LNu01 = new TFile("Gen_ZGstar_massPlot/WZTo3LNu_mllmin01.root");
   TH1F* h_WZ = (TH1F*)f_WZTo3LNu->Get("h_0");
   TH1F* h_01 = (TH1F*)f_WZTo3LNu01->Get("h_0");
+
+  TH1F* h_WZ_scaleCalc = (TH1F*)h_WZ->Clone("h_WZ_scaleCalc");
+  TH1F* h_01_scaleCalc = (TH1F*)h_01->Clone("h_01_scaleCalc");
+  // Scale calculation
   double lumi_WZ = 2000000/4.42965/1000; //fb
   double lumi_01 = 989340 /4.42965/1000; //fb
-  h_WZ->Scale(1./lumi_WZ);
-  h_01->Scale(1./lumi_01);
-  h_WZ->SetLineColor(kRed);
-  h_01->SetLineColor(kBlue);
-  h_01->Draw();
-  h_WZ->Draw("same");
+  h_WZ_scaleCalc->Scale(1./lumi_WZ);
+  h_01_scaleCalc->Scale(1./lumi_01);
+  h_WZ_scaleCalc->SetLineColor(kRed);
+  h_01_scaleCalc->SetLineColor(kBlue);
   double binWidth=120./150.;
-  int bin1= 80/binWidth;
-  int bin2= 100/binWidth;
+  int bin1= 60/binWidth;
+  int bin2= 120/binWidth;
   cout<<bin1<<"  bins  "<<bin2<<endl;
-  double eventN_WZ=h_WZ->Integral(bin1,bin2);
-  double eventN_01=h_01->Integral(bin1,bin2);
+  double eventN_WZ=h_WZ_scaleCalc->Integral(bin1,bin2);
+  double eventN_01=h_01_scaleCalc->Integral(bin1,bin2);
   double scaleWZov01 = eventN_WZ/eventN_01;
-  cout<<scaleWZov01<<endl;
+  cout<<"scale is "<<scaleWZov01<<endl;
   TPaveText *pt = new TPaveText(.3,.5,.85,.8,"NDC");
   //NDC sets  coords relative to pad
   std::ostringstream os;
   os << scaleWZov01;
   std::string str = os.str();
-  string ha="hahaha";
-  TString result= "scale(WZTo3LNu/WZTo3LNu01): 7.9 at Z-peak";
+  TString ts_scaleWZov01=str;
+  TString result= "scale(WZTo3LNu/WZTo3LNu01): "+ts_scaleWZov01+" at Z-peak";
   ///TString *reskult= "scale(WZTo3LNu/WZTo3LNu01): "+str.c_str();
   pt->AddText("Assuming the same cross section to WZTo3LNu(4.42965)");
   pt->AddText("Normalize to be 1/fb");
   TText *txt1 = pt->AddText(result);
   txt1->SetTextSize(0.03);
+
+  // Draw
+  h_01_scaleCalc->Draw();
+  h_WZ_scaleCalc->Draw("same");
   pt->Draw();
 
+  myCan->SaveAs("WZTo3LNuOvWZTo3LNu01_xsecSame.pdf");
+  myCan->SaveAs("WZTo3LNuOvWZTo3LNu01_xsecSame.png");
+
+  lumi_WZ = 2000000/4.42965/1000; //fb
+  lumi_01 = 989340 /4.42965/scaleWZov01/1000; //fb
+  h_WZ->Scale(1./lumi_WZ);
+  h_01->Scale(1./lumi_01);
+  h_WZ->SetLineColor(kRed);
+  h_01->SetLineColor(kBlue);
+  myCan->SetLogy();
+  h_01->Draw();
+  h_WZ->Draw("same");
+  TPaveText *pt2 = new TPaveText(.3,.6,.85,.8,"NDC");
+  double x_secWZ =4.42965;
+  double x_sec01 =4.42965*scaleWZov01;
+  std::ostringstream os1;
+  os1 << x_secWZ;
+  std::string st_x_secWZ = os1.str();
+  std::ostringstream os2;
+  os2 << x_sec01;
+  std::string st_x_sec01 = os2.str();
+  TString ts_x_secWZ = st_x_secWZ;
+  TString ts_x_sec01 = st_x_sec01;
+  TString res_xSecWZ= "x-sec of WZTo3LNu: "+ts_x_secWZ;
+  TString res_xSec01= "x-sec of WZTo3LNu_mllmin01: "+ts_x_sec01;
+  pt2->AddText(res_xSecWZ);
+  pt2->AddText(res_xSec01);
+  pt2->Draw();
   myCan->SaveAs("WZTo3LNuOvWZTo3LNu01.pdf");
   myCan->SaveAs("WZTo3LNuOvWZTo3LNu01.png");
 
