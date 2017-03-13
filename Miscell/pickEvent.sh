8bb97d3e579f3f4e5aa8aa36d211b58bf3079542
SangEun Lee <d4space@gmail.com>
2017-03-13 18:57:45 +0900

    hahahaha

---
+++ Miscell/pickEvent.sh
@@ -0,0 +1,5 @@
+cmsRun pickEvent_cfg.py inputFiles=file:/u/user/sangilpark/RunIISample/ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM/0A1892AC-55C0-E611-8905-0CC47A5FC67D.root \
+  eventsToProcess=1:1078:215316 \
+  #eventsToProcess=123592:334:755009,123592:23:392793,123592:42:79142 \
+  # Run:LumiBlock:Event
+  outputFile=pickedOutPut.root
