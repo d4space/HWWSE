81d400b0cc686a7a419e182a783a5b2eaf85c3d3
SangEun Lee <d4space@gmail.com>
2017-03-14 15:18:28 +0100

    hahahaha

--- Miscell/pickEvent.sh
+++ Miscell/pickEvent.sh
@@ -1,5 +1,12 @@
-cmsRun pickEvent_cfg.py inputFiles=file:/u/user/sangilpark/RunIISample/ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM/0A1892AC-55C0-E611-8905-0CC47A5FC67D.root \
+cmsRun pickEvent_cfg.py inputFiles=file:/afs/cern.ch/user/s/spak/public/ZZZ_sample/0A1892AC-55C0-E611-8905-0CC47A5FC67D.root \
   eventsToProcess=1:1078:215316 \
+  outputFile=pickedOutPut.root
+
+
+
+
   #eventsToProcess=123592:334:755009,123592:23:392793,123592:42:79142 \
   # Run:LumiBlock:Event
-  outputFile=pickedOutPut.root
+#cmsRun pickEvent_cfg.py inputFiles=file:/u/user/sangilpark/RunIISample/ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM/0A1892AC-55C0-E611-8905-0CC47A5FC67D.root \
+
+#cmsRun pickEvent_cfg.py inputFiles=file:/afs/cern.ch/user/s/spak/public/ZZZ_sample/0A1892AC-55C0-E611-8905-0CC47A5FC67D.root \
